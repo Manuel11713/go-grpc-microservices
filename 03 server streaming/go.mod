c82bcd3b44af6dc65135eb4d0a23ad3748f50506
Manuel Escobedo Lopez <manuelfx117@gmail.com>
2022-11-02 11:55:59 -0600

    copy project from unary

---
+++ 03 server streaming/go.mod
@@ -0,0 +1,8 @@
+module github.com/Manuel11713/go-unary
+
+go 1.16
+
+require (
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
+)
---
+++ 03 server streaming/greet/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Manuel11713/go-unary/greet/proto"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr = "0.0.0.0:50051"
+
+func main() {
+	list, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listining on %s \n", addr)
+
+	s := grpc.NewServer()
+
+	pb.RegisterGreetServiceServer(s, &Server{})
+
+	if err = s.Serve(list); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
